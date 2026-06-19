d8f1e0a34d95ed2e5cf259ed0000cb6501d97a24
biscuits310 <edmund.w.taylor@gmail.com>
2026-06-19 20:04:31 +0100

    Tags and loot tables created tags and loot tables applied to REDWOOD_PLANKS and REDWOOD_LOG

--- src/main/java/net/biscuits310/redwoodforests/RedwoodForestsDataGen.java
+++ src/main/java/net/biscuits310/redwoodforests/RedwoodForestsDataGen.java
@@ -1,11 +1,18 @@
 package net.biscuits310.redwoodforests;
 
+import net.biscuits310.redwoodforests.datagen.ModBlockLootTableProvider;
+import net.biscuits310.redwoodforests.datagen.ModBlockTagsProvider;
 import net.biscuits310.redwoodforests.datagen.ModModelProvider;
 import net.minecraft.data.DataGenerator;
 import net.minecraft.data.PackOutput;
+import net.minecraft.data.loot.LootTableProvider;
+import net.minecraft.world.level.storage.loot.parameters.LootContextParamSets;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.neoforge.data.event.GatherDataEvent;
+
+import java.util.Collections;
+import java.util.List;
 
 @EventBusSubscriber(modid = RedwoodForests.MODID)
 public class RedwoodForestsDataGen
@@ -15,8 +22,12 @@ public class RedwoodForestsDataGen
     {
         DataGenerator generator = event.getGenerator();
         PackOutput packOutput = generator.getPackOutput();
+        var lookupProvider = event.getLookupProvider();
 
         generator.addProvider(true, new ModModelProvider(packOutput));
+        generator.addProvider(true, new ModBlockTagsProvider(packOutput, lookupProvider));
+        generator.addProvider(true, new LootTableProvider(packOutput, Collections.emptySet(),
+                List.of(new LootTableProvider.SubProviderEntry(ModBlockLootTableProvider::new, LootContextParamSets.BLOCK)), lookupProvider));
     }
 
 }
---
+++ src/main/java/net/biscuits310/redwoodforests/datagen/ModBlockTagsProvider.java
@@ -0,0 +1,47 @@
+package net.biscuits310.redwoodforests.datagen;
+
+import net.biscuits310.redwoodforests.RedwoodForests;
+import net.biscuits310.redwoodforests.block.ModBlocks;
+import net.minecraft.core.HolderLookup;
+import net.minecraft.data.PackOutput;
+import net.minecraft.tags.BlockTags;
+import net.neoforged.neoforge.common.data.BlockTagsProvider;
+
+import java.util.concurrent.CompletableFuture;
+
+public class ModBlockTagsProvider extends BlockTagsProvider {
+    public ModBlockTagsProvider(PackOutput output, CompletableFuture<HolderLookup.Provider> lookupProvider) {
+        super(output, lookupProvider, RedwoodForests.MODID);
+    }
+
+    @Override
+    protected void addTags(HolderLookup.Provider provider) {
+        tag(BlockTags.SNAPS_GOAT_HORN)
+                .add(ModBlocks.REDWOOD_LOG.get());
+
+        tag(BlockTags.OVERWORLD_NATURAL_LOGS)
+                .add(ModBlocks.REDWOOD_LOG.get());
+
+        tag(BlockTags.LOGS_THAT_BURN)
+                .add(ModBlocks.REDWOOD_LOG.get());
+
+        tag(BlockTags.LAVA_POOL_STONE_CANNOT_REPLACE)
+                .add(ModBlocks.REDWOOD_LOG.get());
+
+        tag(BlockTags.COMPLETES_FIND_TREE_TUTORIAL)
+                .add(ModBlocks.REDWOOD_LOG.get());
+
+        tag(BlockTags.PARROTS_SPAWNABLE_ON)
+                .add(ModBlocks.REDWOOD_LOG.get());
+
+        tag(BlockTags.LOGS)
+                .add(ModBlocks.REDWOOD_LOG.get());
+
+        tag(BlockTags.MINEABLE_WITH_AXE)
+                .add(ModBlocks.REDWOOD_LOG.get())
+                .add(ModBlocks.REDWOOD_PLANKS.get());
+
+        tag(BlockTags.PLANKS)
+                .add(ModBlocks.REDWOOD_PLANKS.get());
+    }
+}
